04465e4194660d148958b29ea70989a42c5ee3c9
github Sarthak4730 <sarthakasm07@gmail.com>
2025-02-05 18:48:01 +0530

    expt 3 completed

--- Expt3Portfolio/script.js
+++ Expt3Portfolio/script.js
@@ -3,20 +3,24 @@ let darkTheme = false;
 let btn = document.querySelector("button");
 let img = document.querySelector("#modeToggle");
 
-btn.addEventListener("click", ()=>{
-    darkTheme = !darkTheme;    
+btn.addEventListener("click", () => {
+  darkTheme = !darkTheme;
 
-    if(darkTheme){
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "white";
-        btn.style.backgroundColor = "greenyellow";
-        btn.style.border = "3px solid white";
-        img.setAttribute("src", "sun.svg");
-    }else{
-        document.body.style.backgroundColor = "greenyellow";
-        document.body.style.color = "black";
-        btn.style.backgroundColor = "white";
-        btn.style.border = "3px solid black";
-        img.setAttribute("src", "moon.svg");
-    }
+  if (darkTheme) {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    btn.style.backgroundColor = "#befbff";
+    btn.style.border = "3px solid white";
+    document.querySelector("h2").style.color = "yellow";
+    img.setAttribute("src", "sun.svg");
+    document.querySelector("#logo").style.filter = "invert(1)";
+  } else {
+    document.body.style.backgroundColor = "#befbff";
+    document.body.style.color = "black";
+    btn.style.backgroundColor = "white";
+    btn.style.border = "3px solid black";
+    document.querySelector("h2").style.color = "#00879d";
+    img.setAttribute("src", "moon.svg");
+    document.querySelector("#logo").style.filter = "invert(0)";
+  }
 });
